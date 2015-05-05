2568e8e27dc0d0befa747abeb4c70f0242ef7e9c
Jarry <jlzhu@ichint.com>
2015-05-05 09:50:41 +0800

    Initial Commit

---
+++ SCUtility/SCUtility.h
@@ -0,0 +1,19 @@
+//
+//  SCUtility.h
+//  SCUtility
+//
+//  Created by Jarry on 15/5/5.
+//  Copyright (c) 2015年 iChint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SCUtility.
+FOUNDATION_EXPORT double SCUtilityVersionNumber;
+
+//! Project version string for SCUtility.
+FOUNDATION_EXPORT const unsigned char SCUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCUtility/PublicHeader.h>
+
+
